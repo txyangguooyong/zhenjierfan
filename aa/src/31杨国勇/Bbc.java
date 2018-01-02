35fb9ae33b0c8798b0bb6b4e627d5f90032633a2
txyangguooyong <2416066818@qq.com>
2018-01-02 16:35:32 +0800

    你好烦

---
+++ aa/src/31杨国勇/Bbc.java
@@ -0,0 +1,17 @@
+package Abc;
+
+import java.util.Scanner;
+
+public class Bbc {
+
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	int[] arr=new int[a];
+	for (int i = 0; i < arr.length; i++) {
+		System.out.println(arr[i]);
+	}
+   
+	}
+
+}
